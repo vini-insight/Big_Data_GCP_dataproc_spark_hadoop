51a0f4a4acbb059a7614396c9afd12692e442f1a
Vinicius Vieira <wynny.eu@gmail.com>
2021-08-28 11:27:10 -0300

    add comment
    add comment

--- contador.py
+++ contador.py
@@ -5,3 +5,5 @@ if __name__ == "__main__":
     words = sc.textFile("gs://{NOME_DO_SEU_BUCKET}/livro.txt").flatMap(lambda line: line.split(" "))
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
     wordCounts.saveAsTextFile("gs://{NOME_DO_SEU_BUCKET}/resultado")
+    
+#     OBSERVAÇÃO: por questões de segurança o nome do bucket foi omitido.
